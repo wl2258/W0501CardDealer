19a856d66b582dec50955410d71977d25db84531
Jimin <77067383+wl2258@users.noreply.github.com>
2022-10-05 14:19:24 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20200607/W0501CardDealer/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20200607.W0501CardDealer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
